84972c003f1fb43fc82d9d419c8e7d12d31fa4ab
bbathel12 <brice@amberandbrice.com>
2016-07-05 14:44:21 -0500

    fixed nav

--- application/views/nav.php
+++ application/views/nav.php
@@ -1,6 +1,6 @@
 <div class="row">
   <nav class="navbar navbar-default navbar-fixed-top">
-   <a class="navbar-brand" href="/index.php">Amber and Brice Wedding Song List</a>
+   <a class="navbar-brand" href="<?= base_url() ?>">Amber and Brice Wedding Song List</a>
     <div class="container">
       <ul class="nav navbar-nav">
               <li><a href="<?= base_url() ?>">List</a></li>
